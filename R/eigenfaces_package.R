3f7acc79af82f32a5ba4c9bfb5e13692b20233a4
vbonhomme <bonhomme.vincent@gmail.com>
2015-03-26 12:10:28 +0100

    added pain dataset

--- R/eigenfaces_package.R
+++ R/eigenfaces_package.R
@@ -15,3 +15,16 @@
 #' @importFrom jpeg readJPEG
 NULL
 
+#' Pain expression subset from Stirling (PICS)
+#'
+#' Converted as an afaces object
+#' @docType data
+#' @name pain
+#' @rdname data_pain
+#' @keywords Datasets
+#' @format 84 face images, fixed eye location, 7 expressions (not sad) from each of 12 women.
+#' Resolution: 181x241 monochrome
+#' \describe{an afaces object}
+#' @source "Pain expression subset" found there \url{http://pics.psych.stir.ac.uk/2D_face_sets.htm}
+NULL
+
